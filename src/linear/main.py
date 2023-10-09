7b4bfdadadc3270e5212e589f101efe2d6053e70
Caspar Nettelbladt <caspar.n@gmail.com>
2023-10-09 13:28:37 +0200

    Update linear CLI to list issues based on states
    - Add `list_issues` function to group issues by state
    - Add `issue_list` function to handle the `linear issue list` command
    with options `--all`, `--done`, and `--cancelled`
    - Add `issue_view` function to handle the `linear issue view` command
    with option `--web`
    - Update `cli` function to handle the new subcommands and options

--- src/linear/main.py
+++ src/linear/main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import webbrowser
+from collections import defaultdict
 
 from colorama import Back, Fore, Style, init
 
@@ -20,16 +21,31 @@ def view_issue(issue_id: str, web: bool = False):
     print(f"{issue.url}")
 
 
-def list_issues():
+def list_issues(accepted_states: list[str]):
     init()
     client = linear_client.LinearClient(LINEAR_API_KEY)
     me = client.get_me()
-    in_progress_issues = [
-        issue for issue in me.assigned_issues if issue.state.name == "In Progress"
+    # Group issues by `issue.state.name`
+    issues_by_state = defaultdict(list)
+
+    for item in me.assigned_issues:
+        # if item.state.name in accepted_states:
+        issues_by_state[item.state.name].append(item)
+
+    selected_states = [
+        state for state in issues_by_state.keys() if state in accepted_states
     ]
-    for issue in in_progress_issues:
+
+    if len(selected_states) == 0:
+        print(f"No issues in ({', '.join(accepted_states)})")
+        # Print the count of issues in each stated
+        for state, issues in issues_by_state.items():
+            print(f"{state} ({len(issues)})")
+        return
+
+    for state, issues in issues_by_state.items():
         # title_color = Fore.GREEN if issue.state.name == "Done" else Fore.RED
-        match issue.state.name:
+        match state:
             case "Done":
                 status_color = Fore.GREEN
             case "In Progress":
@@ -37,12 +53,12 @@ def list_issues():
             case _:
                 status_color = Fore.RED
 
-        status = f"{status_color}{issue.state.name}{Style.RESET_ALL}"
-
-        print(f"{issue.title} ({status})")
-        print(f"{issue.description}")
-        print()
-        print(f"{issue.url}")
+        status = f"{status_color}{state}{Style.RESET_ALL}"
+        for issue in issues:
+            print(f"{issue.title} ({status})")
+            print(f"{issue.description}")
+            print()
+            print(f"{issue.url}")
 
 
 def view_user():
@@ -50,48 +66,74 @@ def view_user():
     pass
 
 
+def issue_list(args):
+    """
+    linear issue list
+    linear issue list --all
+    linear issue list --cancelled
+    linear issue list --completed
+    """
+    all_states = ["In Progress", "Done", "Prioritized Backlog"]
+    accepted_states = ["In Progress"]
+
+    if args.all:
+        accepted_states = all_states
+    elif args.done:
+        accepted_states.append("Done")
+    elif args.cancelled:
+        accepted_states.append("Cancelled")
+    print(accepted_states)
+
+    list_issues(accepted_states)
+
+
+def issue_view(args):
+    """
+    linear issue view <issue_id>
+
+    Options:
+        --web - open issue in browser
+
+    Examples:
+    linear-cli issue view TRA-683
+    """
+
+    if args.web:
+        view_issue(args.issue_id, web=True)
+        return
+
+    view_issue(args.issue_id)
+
+
 def cli():
-    # linear issue list
-    # linear issue view <issue_id>
-
-    # Examples:
-    # linear issue view TRA-331
-    # linear issue view TRA-331 --web
-
-    # linear team list
-    # linear team view <team_id>
-
     parser = argparse.ArgumentParser(description="Linear CLI")
-
     subparsers = parser.add_subparsers(dest="command")
-
     issue_parser = subparsers.add_parser("issue")
+
+    # Issue subcommands
     issue_subparsers = issue_parser.add_subparsers(dest="issue_command")
 
+    # Issue list
     issue_list_parser = issue_subparsers.add_parser("list")
-    issue_list_parser.add_argument("--project", type=str, help="Project ID")
+    issue_list_parser.add_argument("--done", action="store_true", help="Completed")
+    issue_list_parser.add_argument("--all", action="store_true", help="All")
+    issue_list_parser.add_argument("--cancelled", action="store_true", help="Cancelled")
+    issue_list_parser.set_defaults(func=issue_list)
 
+    # Issue view
     issue_view_parser = issue_subparsers.add_parser("view")
     issue_view_parser.add_argument("issue_id", type=str, help="Issue ID")
-
     issue_view_parser.add_argument(
         "--web", action="store_true", help="Open issue in browser"
     )
-
-    team_parser = subparsers.add_parser("team")
-    team_subparsers = team_parser.add_subparsers(dest="team_command")
-
-    team_list_parser = team_subparsers.add_parser("list")
-
-    team_view_parser = team_subparsers.add_parser("view")
+    issue_view_parser.set_defaults(func=issue_view)
 
     args = parser.parse_args()
-
-    if args.command == "issue":
-        if args.issue_command == "list":
-            list_issues()
-        elif args.issue_command == "view":
-            view_issue(args.issue_id, web=args.web)
+    match args:
+        case None:
+            parser.print_help()
+        case _:
+            args.func(args)
 
 
 def main():
