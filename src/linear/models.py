b3501d6d3a287b01fb873f0fbf6644a4f313545c
Caspar Nettelbladt <caspar.n@gmail.com>
2023-10-09 23:41:27 +0200

    Refactor models and queries
    Refactored the `models.py` and `queries.py` files to improve code
    readability and maintainability.
    
    Introduced static methods in the `Issue` and `User` classes to handle
    GraphQL fields and object creation from GraphQL responses.

--- src/linear/models.py
+++ src/linear/models.py
@@ -27,6 +27,38 @@ class Issue:
     url: str
     state: WorkflowState
 
+    @staticmethod
+    def gql_fields() -> str:
+        return """
+            id
+            identifier
+            title
+            createdAt
+            description
+            url
+            state {
+              id
+              name
+              type
+            }
+        """
+
+    @staticmethod
+    def from_gql(issue: dict) -> "Issue":
+        return Issue(
+            id=issue["id"],
+            identifier=issue["identifier"],
+            title=issue["title"],
+            description=issue["description"],
+            created_at=issue["createdAt"],
+            url=issue["url"],
+            state=WorkflowState(
+                id=issue["state"]["id"],
+                name=issue["state"]["name"],
+                type=issue["state"]["type"],
+            ),
+        )
+
 
 @dataclass
 class User:
@@ -37,3 +69,29 @@ class User:
     """
     Issues assigned to the user.
     """
+
+    @staticmethod
+    def gql_fields() -> str:
+        return """
+            id
+            name
+            email
+            assignedIssues {
+                nodes {
+                    %s
+                }
+            }
+        """ % (
+            Issue.gql_fields()
+        )
+
+    @staticmethod
+    def from_gql(viewer: dict) -> "User":
+        return User(
+            id=viewer["id"],
+            name=viewer["name"],
+            email=viewer["email"],
+            assigned_issues=[
+                Issue.from_gql(issue) for issue in viewer["assignedIssues"]["nodes"]
+            ],
+        )
