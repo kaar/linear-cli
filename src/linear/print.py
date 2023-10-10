e1314900ba8359cb5891abc01533c2b29656cb0a
Caspar Nettelbladt <caspar.n@gmail.com>
2023-10-10 23:53:18 +0200

    Refactor issue printing

--- src/linear/print.py
+++ src/linear/print.py
@@ -6,7 +6,7 @@ from pygments import highlight
 from pygments.formatters import TerminalFormatter
 from pygments.lexers.markup import MarkdownLexer
 
-from .models import Issue
+from .models import Comment, Issue
 
 init()
 
@@ -33,6 +33,41 @@ def date_format(date: str):
     return f" ({datetime_parsed.strftime('%Y-%m-%d - %H:%M:%S')})"
 
 
+def subissue_as_formatted_text(issue: Issue):
+    formated_text = ""
+
+    match issue.state.name:
+        case "Done":
+            status_color = Fore.GREEN
+        case "In Progress":
+            status_color = Fore.YELLOW
+        case "Prioritized backlog":
+            status_color = Fore.BLUE
+        case _:
+            status_color = Fore.RED
+
+    status = f"{status_color}{issue.state.name}{Style.RESET_ALL}"
+    title = f"{issue.title} {issue.identifier}"
+    meta_data = f"({status})"
+    formated_text += f"{title}\n"
+    formated_text += f"{meta_data}\n"
+
+    return formated_text
+
+
+def comments_as_formatted_text(comments: list[Comment]) -> str:
+    comment_text = "\n"
+    for comment in comments:
+        comment_text += (
+            f"{Fore.BLUE}"
+            f"{comment.user_name}{date_format(comment.created_at)}"
+            f"{Style.RESET_ALL}\n"
+        )
+        comment_text += markdown_format(comment.body)
+
+    return comment_text
+
+
 def issue_as_formatted_text(
     issue: Issue,
     show_description: bool = False,
@@ -51,45 +86,35 @@ def issue_as_formatted_text(
             status_color = Fore.RED
 
     status = f"{status_color}{issue.state.name}{Style.RESET_ALL}"
-    title = f"{issue.title} {issue.identifier}"
-    meta_data = f"({status})"
+    title = f"({status}) {issue.title} {issue.identifier}"
 
-    issue_text += f"{title}\n"
-    issue_text += f"{meta_data}\n"
+    issue_text += f"{title}"
 
     if show_description:
         if issue.description:
             description = issue.description.replace("\n\\", "\n")
-            issue_text += markdown_format(description)
+            formatted_description = markdown_format(description)
+            issue_text += f"\n{formatted_description}"
 
     if show_subissues := issue.children:
         issue_text += "Sub-issues:\n"
         # print("Sub-issues:")
         for subissue in show_subissues:
-            issue_as_formatted_text(subissue, show_description=False)
+            subissue_text = subissue_as_formatted_text(subissue)
+            issue_text += subissue_text
 
-    comment_text = ""
     if issue.comments:
+        comment_text = ""
         if show_comments:
-            # comment_text = "\nComments:\n"
-            comment_text = "\n"
-            for comment in issue.comments:
-                comment_text += (
-                    f"{Fore.BLUE}"
-                    f"{comment.user_name}{date_format(comment.created_at)}"
-                    f"{Style.RESET_ALL}\n"
-                )
-                comment_text += markdown_format(comment.body)
-                issue_text += comment_text
-
+            comment_text = comments_as_formatted_text(issue.comments)
         else:
             comment_text = (
                 f"{Fore.RED}"
                 f"--- Not showing {len(issue.comments)} comments ---"
                 f"{Style.RESET_ALL}"
             )
-            issue_text += comment_text
-            # issue_text += f"{Fore.RED}{comment_text}{Style.RESET_ALL}"
+
+        issue_text += comment_text
 
     url = f"{Fore.GREEN}" f"{issue.url}" f"{Style.RESET_ALL}"
     issue_text += f"\n{url}"
